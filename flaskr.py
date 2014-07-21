f7239f8683776f3aefeb5b1f9e4e20597a677a05
Daye Kim <daye10003@gmail.com>
2014-07-21 19:13:14 +0900

    7.2

--- flaskr.py
+++ flaskr.py
@@ -19,6 +19,13 @@ def connect_db():
 	rv.row_factory=sqlite3.Row
 	return rv
 
+def init_db():
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
 def get_db():
 	# Opens a new database connection if there is none yet for the
 	# current application context.
@@ -32,18 +39,12 @@ def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
 
-def init_db():
-	with app.app_context():
-		db = get_db()
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
-
+@app.route('/')
 def show_entries():
 	db = get_db()
 	cur = db.execute('select title, text from entries order by id desc')
 	entries = cur.fetchall()
-	return render_template('show_entries.html', entries = entries)
+	return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -53,9 +54,9 @@ def add_entry():
 	db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
 	db.commit()
 	flash('New entry was successfully posted')
-	return redirect(url_for('show_entries')
+	return redirect(url_for('show_entries'))
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
 	error = None 
 	if request.method == 'POST':
@@ -74,7 +75,8 @@ def logout():
 	session.pop('logged_in', None)
 	flash('You were logged out')
 	return redirect(url_for('show_entries'))
-	
+
 
 if __name__ == '__main__':
+	init_db()
 	app.run()
